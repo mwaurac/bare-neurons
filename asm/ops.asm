607652ca37fd9b7555b26221b9f6d59f7acc492b
Collins Mwaura <cmwambui@proton.me>
2026-08-29 16:12:47 +0300

    feat: added relu backward kernel and the test

--- asm/ops.asm
+++ asm/ops.asm
@@ -36,3 +36,42 @@ relu_forward:
 .rf_tail_done:
   vzeroupper
   ret
+
+; void relu_backward(const float *grad_in, const float *z, float *grad_out, i64 n)
+; rdi=grad_in, rsi = z, rdx = grad_out, rcx = n 
+;
+;grad_out[i] = z[i] > 0 ? grad_in[i] : 0 
+;
+global relu_backward
+relu_backward:
+  vxorps ymm15, ymm15, ymm15
+  mov rax, rcx
+  and rax,  -8
+  xor r10, r10
+
+.rb_vec:
+  cmp r10, rax
+  jge .rb_vec_done
+  vmovups ymm0, [rdi + r10*4]
+  vmovups ymm1, [rsi + r10*4]
+  vcmpps ymm2, ymm1, ymm15, 14
+  vandps ymm0, ymm0, ymm2
+  vmovups [rdx + r10*4], ymm0
+  add r10, 8
+  jmp .rb_vec
+
+.rb_vec_done:
+.rb_tail:
+  cmp r10, rcx
+  jge .rb_tail_done 
+  vmovss xmm0, [rdi + r10*4]
+  vmovss xmm1, [rsi + r10*4]
+  vcmpss xmm2, xmm1, xmm15, 14
+  vandps xmm0, xmm0, xmm2
+  vmovss [rdx + r10*4], xmm0
+  inc r10
+  jmp .rb_tail
+
+.rb_tail_done:
+  vzeroupper
+  ret
--- src/ffi.rs
+++ src/ffi.rs
@@ -1,5 +1,7 @@
 unsafe extern "C" {
     pub unsafe fn relu_forward(z: *const f32, a: *mut f32, n: i64);
+
+    pub unsafe fn relu_backward(grad_in: *const f32, z: *const f32, grad_out: *mut f32, n: i64);
 }
 
 #[cfg(test)]
@@ -18,5 +20,24 @@ mod test {
         let expected = [0.0f32, 0.0, 0.0, 2.0, 5.5, 0.0, 8.0, 1.0, 0.0];
 
         assert_eq!(a, expected);
+    }
+
+    #[test]
+    fn relu_backward_works() {
+        let z = [-3.0f32, -1.5, 0.0, 2.0, 5.5, -7.0, 8.0, 1.0, -9.0];
+        let grad_in = [1.0f32, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+        let expected = [0.0f32, 0.0, 0.0, 4.0, 5.0, 0.0, 7.0, 8.0, 0.0];
+        let mut grad_out = [0.0f32; 9];
+
+        unsafe {
+            relu_backward(
+                grad_in.as_ptr(),
+                z.as_ptr(),
+                grad_out.as_mut_ptr(),
+                z.len() as i64,
+            );
+        }
+
+        assert_eq!(grad_out, expected);
     }
 }
